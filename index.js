6702f4138ddc68b741f6d1e8375566035b109da2
yanamalareddy <64394449+yanamalareddy@users.noreply.github.com>
2020-04-27 12:01:34 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -15,23 +15,22 @@ function play1(){
  document.getElementById("user").innerHTML="<img src='imgs/1.jpg' height='150' width='150'>";
  score();
  document.getElementById('score1').innerHTML=total;
- console.log(randomimages());
+ randomimages();
  
+
 }
 function play2(){
    
     uchoice=2;
     
-    document.getElementById("user").innerHTML="<img src='imgs/2.webp' height='150' width='150'>";
+    document.getElementById("user").innerHTML="<img src='imgs/2.jpg' height='150' width='150'>";
     score();
     document.getElementById('score1').innerHTML=total;
            
-   
+  randomimages(); 
        
        
-    randomimages();
-
-          
+  
    }
 
    function play3(){
@@ -50,7 +49,7 @@ function play2(){
    
     uchoice=4;
     
-    document.getElementById("user").innerHTML="<img src='imgs/4.webp' height='150' width='150'>";
+    document.getElementById("user").innerHTML="<img src='imgs/4.jpg' height='150' width='150'>";
     score();
     document.getElementById('score1').innerHTML=total;
     randomimages();
@@ -70,7 +69,7 @@ function play2(){
    
     uchoice=6;
     
-    document.getElementById("user").innerHTML="<img src='imgs/6.webp' height='150' width='150'>";
+    document.getElementById("user").innerHTML="<img src='imgs/6.jpg' height='150' width='150'>";
     score();
     document.getElementById('score1').innerHTML=total;
     randomimages();
@@ -80,21 +79,26 @@ function play2(){
 function randomimages(){
 var myimages=[];
 myimages[1]="imgs/1.jpg";
-myimages[2]="imgs/2.webp";
+myimages[2]="imgs/2.jpg";
 myimages[3]="imgs/3.jpg";
-myimages[4]="imgs/4.webp";
+myimages[4]="imgs/4.jpg";
 myimages[5]="imgs/5.jpg";
-myimages[6]="imgs/6.webp";
+myimages[6]="imgs/6.jpg";
 var rnd=Math.floor(Math.random()*myimages.length);
 if(rnd==0){
     rnd=1;
 }
 
 
-document.write('<img src="'+myimages[rnd]+'"height="150" width="150">');
+var imgss=document.createElement("img");
+imgss.setAttribute("src",myimages[rnd]);
+imgss.setAttribute("height","150");
+imgss.setAttribute("width","150");
+document.getElementById("bt").appendChild(imgss);
+
+//document.create('<img src="'+myimages[rnd]+'"height="150" width="150">');
 
 }
-
 
 
 
